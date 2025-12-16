f12d47e991de3acc85ccd3d7ff624493bc0fdca5
Matheus Vicente <vicentematheus1507@gmail.com>
2025-12-15 22:52:17 -0300

    feat: add movimentacao routes for stock entry, exit, and history management

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,20 @@ const cors = require('cors');
 
 const authRoutes = require('./routes/auth.routes');
 const produtoRoutes = require('./routes/produto.routes');
+const estoqueRoutes = require('./routes/estoque.routes');
+const movimentacaoRoutes = require('./routes/movimentacao.routes');
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.use('/auth', authRoutes);
 app.use('/produtos', produtoRoutes);
+app.use('/estoque', estoqueRoutes);
+app.use('/movimentacoes', movimentacaoRoutes);
+
 
 module.exports = app;
