cfb7ecd7301a5b323e5b7f7ad2d98e3a0718acba
Matheus Vicente <vicentematheus1507@gmail.com>
2025-12-16 15:32:13 -0300

    fix: update API baseURL to correct IP address for service connection

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://SEU_IP:3333'
+  baseURL: 'http://192.168.0.2:3333'
+            //'http://localhost:3333'
 });
 
 export default api;
