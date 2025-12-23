22f2d7491ed6a6d0c6e059f80fc3f014fed227cc
Matheus Vicente <vicentematheus1507@gmail.com>
2025-12-22 22:31:02 -0300

    fix: update commented-out baseURL in api.js for clarity

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://192.168.0.2:3333'
-  //http://localhost:3333
-});
+  //  http://localhost:3333 para testar com o axpo pelo celular
 
 export default api;
